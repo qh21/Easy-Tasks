ce281f31a94edac5abb22ac9246269d7f6826e8b
Qassem Al-Hanbali <76710890+qh21@users.noreply.github.com>
2024-06-30 14:01:29 +0300

    Create printYourName.cpp

---
+++ Print Your Name/printYourName.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+    string yourName;
+    cout << "Please Enter Your Name:\t";
+    cin >> yourName;
+    cout <<endl<< "Hello "<<yourName<<endl;
+
+}
